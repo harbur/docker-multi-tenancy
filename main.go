69cd85f60f798d5e4b39c90a590bc3df4420d442
morfeo8marc <morfeo8marc@gmail.com>
2015-09-18 18:01:26 +0200

    Add basic connection through unix socket

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fsouza/go-dockerclient"
+)
+
+func main(){
+
+	fmt.Println("Starting multi-tenancy proxy")
+
+	endpoint := "unix:///var/run/docker.sock"
+	client, _ := docker.NewClient(endpoint)
+	imgs, _ := client.ListImages(docker.ListImagesOptions{All: false})
+	for _, img := range imgs {
+		fmt.Println("ID: ", img.ID)
+		fmt.Println("RepoTags: ", img.RepoTags)
+		fmt.Println("Created: ", img.Created)
+		fmt.Println("Size: ", img.Size)
+		fmt.Println("VirtualSize: ", img.VirtualSize)
+		fmt.Println("ParentId: ", img.ParentID)
+	}
+}
